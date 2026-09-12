175db4af0317f118a9cbebf07938904769b82f6a
Zeejay Tan <217020377+zeejaytan@users.noreply.github.com>
2026-09-12 15:32:11 +1000

    Fix step 2a import path from scripts dir

--- scripts/render_a02_gsaug_step2a.sh
+++ scripts/render_a02_gsaug_step2a.sh
@@ -11,6 +11,7 @@ module load GCC/11.3.0 OpenMPI/4.1.4 CUDA/11.8.0
 export CUDA_HOME="${EBROOTCUDA:?EBROOTCUDA missing after module load}"
 export LD_LIBRARY_PATH="$CUDA_HOME/targets/x86_64-linux/lib:${LD_LIBRARY_PATH:-}"
 cd /data/gpfs/projects/punim2657/MILo/repo/milo
+export PYTHONPATH="/data/gpfs/projects/punim2657/MILo/repo/milo:${PYTHONPATH:-}"
 /data/gpfs/projects/punim2657/MILo/envs/milo/bin/python ../scripts/render_novel_sh5.py \
     -s /data/gpfs/projects/punim2657/MILo/data/17062025/A02 \
     -m /data/gpfs/projects/punim2657/MILo/output/17062025/A02 \
